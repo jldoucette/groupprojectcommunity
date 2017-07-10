2528014019fa735a1d052085346819fb7f9acd4d
Tri Ton <triton@MacBook-Pro.local>
2017-07-09 18:03:43 -0700

    updated handlebars

--- controller/communitycontroller.js
+++ controller/communitycontroller.js
@@ -3,7 +3,29 @@ var db = require('../models');
 module.exports = function(app){
 
     app.get('/', function(req, res) {
-        res.render('index');
+        res.render('index'); //home page
     });
+
+    app.get('/profile', function(req, res) {
+        res.render('profile');
+    });
+
+    app.get('/blog', function(req, res) {
+        res.render('blog');
+    });
+
+    app.get('/events', function(req, res) {
+        res.render('events');
+    });
+
+    app.get('/newsletter', function(req, res) {
+        res.render('newsletters');
+    });
+
+    app.get('/classifieds', function(req, res) {
+        res.render('classified');
+    });
+
+
 }
 
