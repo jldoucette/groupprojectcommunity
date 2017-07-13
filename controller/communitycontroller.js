2d1fc1a8135bd617818d0add2a3a8698e0f05ccf
sugapop35 <oliver9324@gmail.com>
2017-07-12 21:20:43 -0700

    added validation box for profile creation

--- controller/communitycontroller.js
+++ controller/communitycontroller.js
@@ -203,24 +203,21 @@ module.exports = function(app){
 
 
     //create new Users STILL NEED TO MAKE THE VALUES APPROPRIATE WITH TEXT BOX
-    app.post("/newUser", function(req, res) {
-      var AlteredPassword = req.body.userPassword;
-      bcrypt.hash(AlteredPassword, saltRounds, function(err, hash) { //bcrypt encrypts the password
-        db.profile.create({
-            user_name: req.body.userName,
-            user_age: req.body.userAge,
-              user_email: req.body.userEmail,
-              user_password: hash,
-                user_bio: req.body.userBio,
-          complete: req.body.complete
-        }).then(function(dbTodo) {
-            res.json(dbTodo);//to see if message is sent through
-            // res.redirect("/"); //trying to redirect but not working
-            //if successful pop up an account that says you are successful
-            //if failed, pop up box says an account was not created
-        });
-        console.log(req);
+   app.post("/newUser", function(req, res) {
+    var AlteredPassword = req.body.userPassword;
+     bcrypt.hash(AlteredPassword, saltRounds, function(err, hash) { //bcrypt encrypts the password
+      db.profile.create({
+          user_name: req.body.userName,
+          user_age: req.body.userAge,
+            user_email: req.body.userEmail,
+            user_password: hash,
+              user_bio: req.body.userBio,
+        complete: req.body.complete
       });
-    });
+    }).then(function(dbTodo) {
+          res.redirect("/"); //trying to redirect but not working
+
+      });;
+  })
 
 }
