ca0932a7c690c74a6dc37a0812a3cbaabe558d2b
gnguyendo <gnguyendo@gmail.com>
2017-07-14 17:10:02 -0700

    geoffrey4 html fix

--- controller/blogcontroller.js
+++ controller/blogcontroller.js
@@ -1,52 +1,54 @@
-var db = require("../models");
-var BlogId;
-module.exports = function(app) {
+// var db = require("../models");
+// var BlogId;
+// module.exports = function(app) {
 
-  app.get("/", function(req, res) {
+//   app.get("/", function(req, res) {
 
-    db.Blogs.findAll({
+//     db.Blogs.findAll({
 
-       include: [db.Comments],
-       order: [ 'id',
-              [db.Comments, 'createdAt', 'DESC']]
-    }).then(function(data) {
-        var hbsObject = {
-      Blog: data
-    };
-     res.render("blog", hbsObject);
-    });
-  });
+//        include: [db.Comments],
+//        order: [ 'id',
+//               [db.Comments, 'createdAt', 'DESC']]
+//     }).then(function(data) {
+//         var hbsObject = {
+//       Blog: data
+//     };
+//      res.render("blog", hbsObject);
+//     });
+//   });
 
-  app.get("/classifieds", function(req, res) {
+//   app.get("/classifieds", function(req, res) {
 
-    db.Classifieds.findAll({
-       order: ['id']
-    }).then(function(data) {
-        var hbsObject = {
-      Classified: data
-    };
-     res.render("classified", hbsObject);
-    });
-  });
+//     db.Classifieds.findAll({
+//        order: ['id']
+//     }).then(function(data) {
+//         var hbsObject = {
+//       Classified: data
+//     };
+//      res.render("classified", hbsObject);
+//     });
+//   });
 
-  app.post("/", function(req, res) {
-    console.log(req.body);
-    db.Blogs.create({
-      user: req.body.user,
-      blogtitle: req.body.blogtitle,
-      blogpost: req.body.blogpost
-    }).then(function(data) {
-      res.redirect("/");
-    });
-  });
+//   app.post("/", function(req, res) {
+//     console.log(req.body);
+//     db.Blogs.create({
+//       user: req.body.user,
+//       blogtitle: req.body.blogtitle,
+//       blogpost: req.body.blogpost
+//     }).then(function(data) {
+//       res.redirect("/");
+//     });
+//   });
 
- app.post("/comment", function(req, res) {
-    console.log(req.body);
-    db.Comments.create({
-      commentpost: req.body.comment,
-      BlogId: req.body.currblogid,
-      user:req.body.currbloguser
-    }).then(function(data) {
-      res.redirect("/");
-    });
-  });
+//  app.post("/comment", function(req, res) {
+//     console.log(req.body);
+//     db.Comments.create({
+//       commentpost: req.body.comment,
+//       BlogId: req.body.currblogid,
+//       user:req.body.currbloguser
+//     }).then(function(data) {
+//       res.redirect("/");
+//     });
+//   });
+
+// }
