7921217f3bf2b1d4ffac04abed1e69d64bb7b828
jldoucette <jd@jonathandoucette.com>
2017-07-14 21:58:01 -0700

    Changes to chatroom and logout function

--- controller/communitycontroller.js
+++ controller/communitycontroller.js
@@ -14,6 +14,7 @@ module.exports = function(app){
 
      app.get("/logout", function(req, res) {
        userLoggedIn=false;
+       res.redirect("/");
 
       });
 
@@ -177,7 +178,10 @@ module.exports = function(app){
   
     app.get('/chatroom', function(req, res) {
        if (userLoggedIn) {
-        res.render('chatroom'); 
+        var hbsObject={
+          username: siteUsername
+        }
+        res.render('chatroom',hbsObject); 
       }
                else {
     console.log("failed if, no username");
